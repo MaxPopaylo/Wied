3bd3af6589c9dde6050db7a4cef33fc731ddba62
Maksym <max.popaylo@gmail.com>
2025-02-28 14:26:32 +0200

    chore/rewrite-auth-screens chore: rewrite SignInScreen

--- app/src/main/java/ua/wied/presentation/screens/auth/SignInScreen.kt
+++ app/src/main/java/ua/wied/presentation/screens/auth/SignInScreen.kt
@@ -5,11 +5,15 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,12 +22,14 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import ua.wied.R
+import ua.wied.presentation.common.composable.BaseTextField
 import ua.wied.presentation.common.composable.PasswordTextField
-import ua.wied.presentation.common.composable.PhoneTextField
 import ua.wied.presentation.common.composable.PrimaryButton
 import ua.wied.presentation.common.composable.SecondaryButton
 import ua.wied.presentation.common.navigation.AuthNav
@@ -40,7 +46,7 @@ fun SignInScreen(
     val keyboardController = LocalSoftwareKeyboardController.current
     val state = authViewModel.state.signIn
 
-    val enableButton = state.phone.isNotEmpty() && state.password.isNotEmpty()
+    val enableButton = state.login.isNotEmpty() && state.password.isNotEmpty()
 
     Column(
         modifier = Modifier
@@ -52,16 +58,16 @@ fun SignInScreen(
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
+                .fillMaxHeight(0.2f)
                 .background(
-                    colors.secondaryBackground,
-                    RoundedCornerShape(12.dp)
+                    colors.secondaryBackground
                 )
                 .padding(16.dp)
                 .padding(top = 12.dp),
             contentAlignment = Alignment.Center
         ) {
             Column(
-                verticalArrangement = Arrangement.spacedBy(16.dp)
+                verticalArrangement = Arrangement.spacedBy(12.dp)
             ) {
                 Image(
                     modifier = Modifier
@@ -74,7 +80,7 @@ fun SignInScreen(
                 Text(
                     text = stringResource(R.string.welcome),
                     color = colors.primaryText,
-                    style = typography.w400.copy(
+                    style = typography.w500.copy(
                         fontSize = 15.sp
                     )
                 )
@@ -86,29 +92,55 @@ fun SignInScreen(
                 .fillMaxWidth()
                 .wrapContentHeight()
                 .background(
-                    colors.secondaryBackground,
-                    RoundedCornerShape(12.dp)
+                    colors.secondaryBackground
                 )
                 .padding(horizontal = 16.dp, vertical = 24.dp),
             verticalArrangement = Arrangement.spacedBy(32.dp)
         ) {
             Column(
-                verticalArrangement = Arrangement.spacedBy(14.dp)
+                verticalArrangement = Arrangement.spacedBy(16.dp)
             ) {
-                PhoneTextField (
-                    title = stringResource(R.string.phone),
-                    text = state.phone,
-                    errorMessage = state.phoneError?.let { stringResource(it) },
+                Row {
+                    PrimaryButton (
+                        modifier = Modifier.weight(1f),
+                        title = stringResource(R.string.signin),
+                        onClick = {
+                            keyboardController?.hide()
+                        }
+                    )
+
+                    Spacer(modifier = Modifier.width(10.dp))
+
+                    SecondaryButton (
+                        modifier = Modifier.weight(1f),
+                        title = stringResource(R.string.signup),
+                        onClick = {
+                            keyboardController?.hide()
+                            authViewModel.clearAll()
+                            navController.navigate(AuthNav.SignUp.route)
+                        }
+                    )
+                }
+
+                BaseTextField(
+                    title = stringResource(R.string.login),
+                    text = state.login,
+                    fieldColor = colors.primaryBackground,
+                    errorMessage = state.loginError?.let { stringResource(it) },
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Text,
+                        imeAction = ImeAction.Done
+                    ),
                     onValueChange = {
-                        authViewModel.onEvent(SignInUiEvent.PhoneChanged(it))
+                        authViewModel.onEvent(SignInUiEvent.LoginChanged(it))
                     }
                 )
 
                 PasswordTextField (
                     title = stringResource(R.string.password),
                     text = state.password,
+                    fieldColor = colors.primaryBackground,
                     textButton = stringResource(R.string.forgot_password_answer),
-                    description = stringResource(R.string.enter_password),
                     errorMessage = state.passwordError?.let { stringResource(it) },
                     onTextButtonClick = {},
                     onValueChange = {
@@ -117,27 +149,14 @@ fun SignInScreen(
                 )
             }
 
-            Column(
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                PrimaryButton (
-                    title = stringResource(R.string.login),
-                    isEnabled = enableButton,
-                    onClick = {
-                        keyboardController?.hide()
-                        authViewModel.onEvent(SignInUiEvent.SignInClicked)
-                    }
-                )
-
-                SecondaryButton (
-                    title = stringResource(R.string.signup),
-                    onClick = {
-                        keyboardController?.hide()
-                        authViewModel.clearAll()
-                        navController.navigate(AuthNav.SignUp.route)
-                    }
-                )
-            }
+            PrimaryButton (
+                title = stringResource(R.string.signin),
+                isEnabled = enableButton,
+                onClick = {
+                    keyboardController?.hide()
+                    authViewModel.onEvent(SignInUiEvent.SignInClicked)
+                }
+            )
         }
 
         Box(
@@ -145,9 +164,9 @@ fun SignInScreen(
                 .fillMaxWidth()
                 .weight(1f)
                 .background(
-                    colors.secondaryBackground,
-                    RoundedCornerShape(12.dp)
+                    colors.secondaryBackground
                 )
         )
     }
 }
+
