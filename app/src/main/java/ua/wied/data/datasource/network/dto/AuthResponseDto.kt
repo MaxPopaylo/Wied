caa82147bab713472731071f8e8a4ccb9cd6cebd
Maksym <max.popaylo@gmail.com>
2025-03-11 13:40:46 +0200

    chore/rewrite-auth-screens feature: rewrite dto's for new User model

--- app/src/main/java/ua/wied/data/datasource/network/dto/AuthResponseDto.kt
+++
@@ -1,8 +0,0 @@
-package ua.wied.data.datasource.network.dto
-
-import ua.wied.domain.models.user.User
-
-data class AuthResponseDto (
-    val user: User,
-    val token: String
-)
---
+++ app/src/main/java/ua/wied/data/datasource/network/dto/SignInResponseDto.kt
@@ -0,0 +1,5 @@
+package ua.wied.data.datasource.network.dto
+
+data class SignInResponseDto (
+    val data: UserDto
+)
