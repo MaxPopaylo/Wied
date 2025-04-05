0553f104ce2d45c551f596da95e9bfdbebf2aa42
Maksym <max.popaylo@gmail.com>
2025-04-05 18:05:18 +0300

    feature/add-instruction-list-screen: fix InstructionItem

--- app/src/main/java/ua/wied/presentation/common/composable/InstructionItem.kt
+++ app/src/main/java/ua/wied/presentation/common/composable/InstructionItem.kt
@@ -6,13 +6,16 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
@@ -24,16 +27,19 @@ import ua.wied.R
 import ua.wied.domain.models.instruction.Instruction
 import ua.wied.presentation.common.theme.WiEDTheme.colors
 import ua.wied.presentation.common.theme.WiEDTheme.typography
+import ua.wied.presentation.common.utils.bounceClick
 
 @Composable
 fun InstructionItem(
     modifier: Modifier = Modifier,
     instruction: Instruction,
-    actions: @Composable () -> Unit
+    onClick: () -> Unit = {},
+    actions: @Composable () -> Unit = { EmptyAction() }
 ) {
     Row(
         modifier = modifier
             .fillMaxWidth()
+            .bounceClick(onClick)
             .background(
                 colors.secondaryBackground,
                 RoundedCornerShape(4.dp)
@@ -66,4 +72,14 @@ fun InstructionItem(
 
         actions()
     }
+}
+
+@Composable
+private fun EmptyAction() {
+    Icon(
+        modifier = Modifier.rotate(180f).size(25.dp),
+        painter = painterResource(R.drawable.icon_arrow_back),
+        tint = colors.tintColor,
+        contentDescription = stringResource(R.string.icon)
+    )
 }
--- app/src/main/java/ua/wied/presentation/screens/main/instructions/composable/InstructionListItem.kt
+++ app/src/main/java/ua/wied/presentation/screens/main/instructions/composable/InstructionListItem.kt
@@ -4,29 +4,59 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import ua.wied.R
 import ua.wied.domain.models.instruction.Instruction
-import ua.wied.presentation.common.composable.IconButton
+import ua.wied.presentation.common.composable.ActionIcon
 import ua.wied.presentation.common.composable.InstructionItem
+import ua.wied.presentation.common.composable.SwipeableItemWithActions
 import ua.wied.presentation.common.theme.WiEDTheme.colors
 
 @Composable
 fun InstructionListItem(
     modifier: Modifier = Modifier,
     instruction: Instruction,
-    iconOnClick: () -> Unit
+    isRevealed: Boolean,
+    onExpanded: () -> Unit,
+    onCollapsed: () -> Unit,
+    onClick: () -> Unit
 ) {
-    InstructionItem(
-        modifier = modifier,
-        instruction = instruction,
+    SwipeableItemWithActions(
+        isRevealed = isRevealed,
+        onExpanded = onExpanded,
+        onCollapsed = onCollapsed,
         actions = {
-            IconButton(
-                icon = painterResource(R.drawable.icon_play),
-                backgroundColor = Color.Transparent,
-                iconColor = colors.tintColor,
-                borderColor = colors.tintColor,
-                onClick = iconOnClick
+
+            ActionIcon(
+                backgroundColor = colors.tintColor.copy(alpha = 0.8f),
+                icon = painterResource(R.drawable.icon_camcorder),
+                tint = Color.White,
+                title = stringResource(R.string.video),
+                onClick = {}
             )
+
+            ActionIcon(
+                backgroundColor = colors.tintColor,
+                icon = painterResource(R.drawable.icon_add_person),
+                tint = Color.White,
+                title = stringResource(R.string.accesses),
+                onClick = {}
+            )
+
+            ActionIcon(
+                backgroundColor = colors.errorColor,
+                icon = painterResource(R.drawable.icon_delete),
+                tint = Color.White,
+                title = stringResource(R.string.delete),
+                onClick = {}
+            )
+
         }
-    )
+    ) {
+        InstructionItem(
+            modifier = modifier,
+            instruction = instruction,
+            onClick = onClick,
+        )
+    }
 }
